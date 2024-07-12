e5f943b3d46f2f29d7693d5779000d75ca868010
afiffadhlurrahman <afif.fadhlurrahman2@gmail.com>
2024-07-12 09:50:42 +0700

    Initial Commit

---
+++ SamplePairProgramming/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SamplePairProgramming
+//
+//  Created by Muhammad Afif Fadhlurrahman on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SamplePairProgramming/SamplePairProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamplePairProgrammingApp.swift
+//  SamplePairProgramming
+//
+//  Created by Muhammad Afif Fadhlurrahman on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SamplePairProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
